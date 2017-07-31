accb43ca6b53f0eaeea889e53791ebb862e728dd
Alex Saunders <acsauk@gmail.com>
2017-07-31 13:28:53 +0100

    Creates GetTasks()

--- models/tasks.go
+++ models/tasks.go
@@ -16,3 +16,26 @@ type Task struct {
 type TaskCollection struct {
   Tasks []Task `json:"items"`
 }
+
+func GetTasks(db *sql.DB) TaskCollection {
+  sql := "SELECT * FROM tasks"
+  rows, err := db.Query(sql)
+  // Exit if the SQL doesn't work
+  if err != nil {
+    panic(err)
+  }
+  // Cleanup when the program exits
+  defer rows.Close()
+
+  result := TaskCollection{}
+  for rows.Next() {
+    task := Task{}
+    err2 := rows.Scan(&task.ID, &task.Name)
+    // Exit if we get an error
+    if err2 != nil {
+      panic(err2)
+    }
+    result.Tasks = append(result.Tasks, task)
+  }
+  return result
+}
