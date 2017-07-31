809341d2e11b271f40ff7749c2935ccc2877474d
Alex Saunders <acsauk@gmail.com>
2017-07-31 13:16:42 +0100

    again, pre-emptively imports soon to exist models and updates route handlers to use the new models

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -5,6 +5,8 @@ import (
   "net/http"
   "strconv"
 
+  "go-todo/models"
+
   "github.com/labstack/echo"
 )
 
@@ -14,16 +16,29 @@ type H map[string]interface{}
 // GetTasks endpoint
 func GetTasks(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
-    return c.JSON(http.StatusOK, "tasks")
+    // Fetch tasks using the GetTasks model
+    return c.JSON(http.StatusOK, models.GetTasks(db))
   }
 }
 
 // PutTask endpoint
 func PutTask(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
-    return c.JSON(http.StatusCreated, H{
-      "created": 123,
-    })
+    // Instantiate a new Task model
+    var task models.Task
+    // Map incoming JSON body to the new Task
+    c.Bind(&task)
+    // Add a task using the PutTask model
+    id, err := models.PutTasks(db, task.name)
+    // Return JSON response if successful
+    if err == nil {
+      return c.JSON(http.StatusCreated, H{
+        "created": id,
+      })
+    // Handle errors
+    } else {
+      return err
+    }
   }
 }
 
@@ -31,8 +46,16 @@ func PutTask(db *sql.DB) echo.HandlerFunc {
 func DeleteTask(db *sql.DB) echo.HandlerFunc {
   return func(c echo.Context) error {
     id, _ := strconv.Atoi(c.Param("id"))
-    return c.JSON(http.StatusOK, H{
-      "deleted": id,
-    })
+    // Use DeleteTask model to delete a task
+    _, err := models.DeleteTask(db, id)
+    // Return a JSON response on success
+    if err == nil {
+      return c.JSON(http.StatusOK, H{
+        "deleted": id,
+      })
+    // Handle errors
+    } else {
+      return err
+    }
   }
 }
