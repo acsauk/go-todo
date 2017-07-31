c4427805cec11d18223b48582eab5c2ce6199619
Alex Saunders <acsauk@gmail.com>
2017-07-31 13:22:32 +0100

    adds Task and TaskCollection to models

--- models/tasks.go
+++ models/tasks.go
@@ -5,3 +5,14 @@ import (
 
   _ "github.com/mattn/go-sqlite3"
 )
+
+// Task - a struct containing Task data
+type Task struct {
+  ID   int `json:"id"`
+  Name String `json:"name"`
+}
+
+// TaskCollection - a struct containing a collection of Tasks
+type TaskCollection struct {
+  Tasks []Task `json:"items"`
+}
