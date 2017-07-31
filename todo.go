26c749007053e0a9dc329f6d13b6db3302dd5b75
Alex Saunders <acsauk@gmail.com>
2017-07-31 12:33:48 +0100

    adds call to initialize db and mirate data in main(), creates the two functions to carry out initialization and migration

--- todo.go
+++ todo.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+  "database/sql"
   "github.com/labstack/echo"
+  "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+  db := initDB("storage.db")
+  migrate(db)
+
   // Create a new instance of Echo
   e := echo.New()
 
@@ -14,4 +19,34 @@ func main() {
 
   // Start as a web server
   e.Start(":8000")
+}
+
+func initDB(filepath string) *sql.DB {
+  db, err := sql.Open("sqlite3", filepath)
+
+  // Check for any db errors then exit
+  if err != nil {
+    panic(err)
+  }
+
+  // If no errors but no db connection then exit
+  if db == nil {
+    panic("db nil")
+  }
+  return db
+}
+
+func migrate(db *sql.DB) {
+  sql := `
+  CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL
+  );
+  `
+
+  _, err := db.Exec(sql)
+  // Exit if something is wrong with the SQL statement
+  if err != nil {
+    panic(err)
+  }
 }
